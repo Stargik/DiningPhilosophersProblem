c56865714a10362db376c699c7765fa0a35e729e
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-03-16 13:48:16 +0200

    Initial Commit

---
+++ DiningPhilosophersProblem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DiningPhilosophersProblem
+//
+//  Created by Aleksandr Starzhynskyi on 16.03.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
